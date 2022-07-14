cb4bc64438a4498954df5d141ab80a9ea7d13df7
suryansH tomar <you@example.com>
2022-07-14 22:52:49 +0530

    first commit

--- src/Components/Eight.jsx
+++ src/Components/Eight.jsx
@@ -9,12 +9,10 @@ import {AiOutlineTwitter} from "react-icons/ai"
 const Eight = () => {
   return (
     <>
-        <footer className="text-gray-400 bg-[#171818] body-font">
+    <footer className="text-gray-400 bg-[#171818] body-font">
   <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-    <a className="flex title-font font-medium items-center md:justify-start justify-center text-white">
-     <ImHome className='text-[#1BDF8D] text-2xl'/>
-      <span className="ml-3 text-xl">Prime Properties</span>
-    </a>
+  <ImHome className='text-[#1BDF8D] text-2xl'/>
+    <span className="ml-3 text-xl">Prime Properties</span>
     <p className="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4"> © 1995-2022
       <a href="https://twitter.com/knyttneve" className="text-gray-500 ml-1" target="_blank" rel="noopener noreferrer"> primeproperties@gmail.com</a>
     </p>
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div class="context">
+    <div className="context">
       <App/>
     </div>
   </React.StrictMode>
